89a617331796eaa09d7752a40564bae0150b3279
Đức An <16853175+andepzai@users.noreply.github.com>
2020-12-02 01:07:50 +0700

    Update endpoint.go

--- endpoint.go
+++ endpoint.go
@@ -1,14 +1,12 @@
 package endpoint
 
-type Data interface{}
-
 type Error struct {
 	Code    string `json:"code"`
 	Message string `json:"message"`
 }
 
 type Endpoint struct {
-	Status string `json:"status"`
-	Data   Data   `json:"data"`
-	Error  Error  `json:"error"`
+	Status string        `json:"status"`
+	Data   interface{}   `json:"data"`
+	Error  Error         `json:"error"`
 }
